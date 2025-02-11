56e33c4323610dbcff964e1b1179ff496951e249
Bartek Wilczek <bwilczek@gmail.com>
2025-02-11 11:08:42 +0100

    Fix one of dates

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ProgressBarForLongProjects from './ProgressBarForLongProjects';
 function App() {
   return (
     <div className="App">
-      <ProgressBarForLongProjects startDate={new Date('2022-11-04')} title="Pełnoletność Andrzeja" endDate={new Date('2033-07-27')} />
+      <ProgressBarForLongProjects startDate={new Date('2022-11-04')} title="Pełnoletność Andrzeja" endDate={new Date('2033-04-27')} />
       <ProgressBarForLongProjects startDate={new Date('2022-11-04')} title="Pełnoletność Wery" endDate={new Date('2036-01-11')} />
       <ProgressBarForLongProjects startDate={new Date('2018-09-10')} title="Kredyt I" endDate={new Date('2033-10-15')} />
       <ProgressBarForLongProjects startDate={new Date('2021-08-11')} title="Kredyt II" endDate={new Date('2037-11-15')} />
